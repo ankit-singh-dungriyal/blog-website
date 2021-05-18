ab8c63595ed24ed7a54c81d57658c0130876005f
Ankit <ankit.dungriyal123@gmail.com>
2021-05-18 09:05:13 +0530

    Deploying the web-app

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,10 +15,25 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const posts = [];
+//connecting to mongoDB server
+mongoose.connect('mongodb+srv://<userName>:<password>@<clusterName>.uej7s.mongodb.net/blogDB',{useNewUrlParser: true, useUnifiedTopology: true});
+
+//creating Schema
+const blogSchema = new mongoose.Schema({
+  title: String,
+  post : String
+});
+
+const Blog = new mongoose.model("Blog", blogSchema);
 
 app.get("/", function(req, res){
-  res.render("home", {homeContent : homeStartingContent, postList: posts, lodash: _});
+  Blog.find({}, function(err, blogsData){
+    if(err){
+      console.log(err);
+    } else{
+      res.render("home",  {homeContent : homeStartingContent, blogs: blogsData, lodash: _})
+    }
+  })
 })
 
 app.get("/about", function(req, res){
@@ -34,29 +48,48 @@ app.get("/compose", function(req, res){
   res.render("compose");
 })
 app.post("/compose", function(req, res){
-  const composeData = {composeTitle:req.body.composeTitle, composePost:req.body.composePost};
-  posts.push(composeData);
-  res.redirect("/")
+  const composeTitle = req.body.composeTitle;
+  const composePost = req.body.composePost;
+
+  const newBlog = new Blog({
+    title : composeTitle,
+    post : composePost
+  });
+
+  newBlog.save(function(err){
+    if(err){
+      console.log(err);
+    } else{
+      res.redirect("/")
+    }
+  })
 })
 
 app.get("/:title", function(req, res){
   let title = _.kebabCase(req.params.title);
-  console.log(title);
-  posts.forEach(function(post){
-    if(_.kebabCase(post.composeTitle) === title){
-      res.render("post", {title:post.composeTitle, post:post.composePost});
+
+  Blog.find({}, function(err, blogsData){
+    if(err){
+      console.log(err);
+    } else{
+      blogsData.forEach(function(blog){
+        if(_.kebabCase(blog.title) === title){
+          res.render("post", {title: blog.title, post: blog.post});
+        }
+      })
     }
   })
 })
 
 
 
+// setting port
 
+let port = process.env.PORT;
+if(port == null || port == ""){
+  port = 3000;
+}
 
-
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server started on port : "+ port);
 });
